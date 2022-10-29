e6ce86869f184a8f529b782d01f870107d1d73b6
ds704 <42025411+ds704@users.noreply.github.com>
2022-10-29 23:18:45 +0200

    User Sammy

--- src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
+++ src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
@@ -13,7 +13,7 @@ public class MySqlConnector {
     public MySqlConnector(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://172.104.240.132:3306/mydb", "root", "DBAMDIN");
+            con = DriverManager.getConnection("jdbc:mysql://172.104.240.132:3306/mydb", "sammy", "password");
         }catch (Exception e){
             System.out.println(e);
         }
